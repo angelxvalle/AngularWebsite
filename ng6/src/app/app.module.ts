258f0ca8e49750567ddf78d958f50c01fe76ccd2
angel <angelxvalle@gmail.com>
2018-08-21 18:39:14 -0400

    finished tutorial

--- ng6/src/app/app.module.ts
+++ ng6/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,7 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     DetailsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule
   ],
